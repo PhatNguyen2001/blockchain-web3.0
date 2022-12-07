7496308f1f29c572d574fe42e379145af1bbcd72
caophat <caophat225@gmail.com>
2022-12-07 23:19:54 +0700

    update

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -20,10 +20,10 @@ const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword,
     >
       <div className="flex flex-col items-center w-full mt-3">
         <div className="display-flex justify-start w-full mb-6 p-2">
-          <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
+          <a href={`https://goerli.etherscan.io/address/${addressFrom}`} target="_blank" rel="noreferrer">
             <p className="text-white text-base">From: {shortenAddress(addressFrom)}</p>
           </a>
-          <a href={`https://ropsten.etherscan.io/address/${addressTo}`} target="_blank" rel="noreferrer">
+          <a href={`https://goerli.etherscan.io/address/${addressTo}`} target="_blank" rel="noreferrer">
             <p className="text-white text-base">To: {shortenAddress(addressTo)}</p>
           </a>
           <p className="text-white text-base">Amount: {amount} ETH</p>
@@ -40,7 +40,7 @@ const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword,
           className="w-full h-64 2xl:h-96 rounded-md shadow-lg object-cover"
         />
         <div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
-          <p className="text-[#37c7da] font-bold">{timestamp}</p>
+          {/* <p className="text-[#37c7da] font-bold">{timestamp}</p> */}
         </div>
       </div>
     </div>
